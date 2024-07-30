b2126efdaed5fef664cfe459b15084746bd309bf
Tatsu-lab324 <175478545+Tatsu-lab324@users.noreply.github.com>
2024-07-30 22:42:18 +0900

    2024-7-30 views.py

--- src/nippo/models.py
+++ src/nippo/models.py
@@ -5,4 +5,7 @@ class NippoModel(models.Model):
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=1000)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} 時間:{self.timestamp}"
 # Create your models here.
